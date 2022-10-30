eb3b792fc00696861ab7a8995a20095a32e56f91
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-10-30 22:02:41 +0100

    new commit

--- hytrix-dashboard/src/main/java/com/lamkirich/hytrixdashboard/HytrixDashboardApplication.java
+++ hytrix-dashboard/src/main/java/com/lamkirich/hytrixdashboard/HytrixDashboardApplication.java
@@ -2,11 +2,12 @@ package com.lamkirich.hytrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@EnableCircuitBreaker
 public class HytrixDashboardApplication {
 
     public static void main(String[] args) {
